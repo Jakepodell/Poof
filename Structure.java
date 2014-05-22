d544ce09986af2db62a44c920f24d3e02203ff99
kevingreer <kevingreer@me.com>
2014-05-22 10:24:31 -0400

    Create Structure.java

---
+++ Structure.java
@@ -0,0 +1,8 @@
+public abstract class Structure
+{
+  public Structure (int x, int y, Image i, Size s) {}
+  
+  public abstract void draw (Graphics2D) {}
+  
+  public abstract void explode () {}
+}
