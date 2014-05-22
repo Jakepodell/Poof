a8af91e3767cbc6475cc3e1e68306aa3d4b052dc
kevingreer <kevingreer@me.com>
2014-05-22 10:21:58 -0400

    Create Ammo.java

---
+++ Ammo.java
@@ -0,0 +1,8 @@
+public class Ammo
+{
+  public void draw (Graphics2D g)
+  public void launch ()
+  public void move () //move lateral & gravitize
+  public void poof ()
+  public Point getLocation ()
+}
