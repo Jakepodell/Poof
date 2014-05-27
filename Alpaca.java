bca64afed343c037ed7c17214065650d7693309f
Jakepodell <Jake@jakepodell.com>
2014-05-27 10:03:18 -0400

    Update Alpaca.java

--- Alpaca.java
+++ Alpaca.java
@@ -1,15 +1,31 @@
+package poof;
+
 import java.awt.Graphics2D;
+import java.awt.Image;
 import java.awt.Point;
+import java.awt.Toolkit;
 
 public class Alpaca{
 	
 	private int power;
 	private int theta;
 	private int ammo;
+	private Image i;
+	private Image mouth;
 	
+	public Alpaca(int ammoAmount, String s, String mouthS){
+		i = Toolkit.getDefaultToolkit().getImage(s);
+		mouth = Toolkit.getDefaultToolkit().getImage(mouthS);
+		ammo=ammoAmount;
+	}
 	
 	public void draw(Graphics2D g){
+		  g.drawImage(i,ARILOC.x,ARILOC.y,null);
+		  g.drawImage(mouth,MOUTHLOC.x,MOUTHLOC.y,null);
 		  
+	}
+	public void drawArrow(Graphics2D g){
+		
 	}
 	public void rorateMouth(){
 		  
@@ -18,7 +34,7 @@ public class Alpaca{
 		  
 	}
 	public Ammo launch(){
-		  
+		  return(new Ammo());
 	}
 	  
 	public static final Point ARILOC = new Point(50,200);
