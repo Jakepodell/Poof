d656706fafe07a64a4c64a07fed2c6652286ceda
kevingreer <kevingreer@me.com>
2014-05-27 10:24:53 -0400

    Update Grassfro.java

--- Grassfro.java
+++ Grassfro.java
@@ -2,6 +2,8 @@ import java.awt.*;
 
 public class Grassfro extends Structure
 {
+	boolean exploded = false;
+	
 	public Grassfro (int x, int y, Image i, Dimension d)
 	{
 		super(x, y, i, d);
@@ -9,10 +11,16 @@ public class Grassfro extends Structure
 
 	public void draw (Graphics2D g)
 	{
-		g.drawImage(i, x, y, (int) d.getWidth(), (int) d.getHeight(), null);
+		if (exploded == false)
+			g.drawImage(i, x, y, (int) d.getWidth(), (int) d.getHeight(), null);
 	}
 
 	public void explode ()
 	{
+		exploded = true;
+		x = 0;
+		y = 0;
+		i = null;
+		d = null;
 	}
 }
