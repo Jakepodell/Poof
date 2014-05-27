18a12a8a0e7c3ff53048157eb881b95ca413c595
johnmottole <johnmottole@gmail.com>
2014-05-27 10:14:57 -0400

    Update Ammo.java

--- Ammo.java
+++ Ammo.java
@@ -1,10 +1,39 @@
+import java.awt.image.BufferedImage;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Point;
 public class Ammo
 {
-  
-  public Ammo (int angle, int power)
-  public void draw (Graphics2D g)
-  public void launch ()
-  public void move () //move lateral & gravitize
-  public void poof ()
-  public Point getLocation ()
+	BufferedImage theImage;
+	int xSize,ySize, xVelocity, yVelocity;
+	Point theLocation;
+	public Ammo(BufferedImage im, Point loc, int xS,int yS)
+	{
+		theImage = im;
+		theLocation = loc;
+		xSize = xS;
+		ySize = yS;
+		xVelocity = 0;
+		yVelocity = 0;
+
+	}
+	public void draw(Graphics2D g)
+	{
+		g.drawImage(theImage, (int)theLocation.getX(), (int)theLocation.getY(), xSize, ySize, null);
+	}
+	public void launch(int xVel, int yVel)
+	{
+		xVelocity = xVel;
+		yVelocity = yVel;
+	}
+	public void move()
+	{
+		yVelocity -= 10;
+		theLocation.setLocation(theLocation.getX() + xVelocity,theLocation.getY() + yVelocity);
+	}
+	public Point getLocation()
+	{
+		return theLocation;
+	}
+
 }
