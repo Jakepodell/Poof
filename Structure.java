fca1467e006b5a18958bcdb8f82411caa5e25ffb
kevingreer <kevingreer@me.com>
2014-05-27 10:18:13 -0400

    Update Structure.java

--- Structure.java
+++ Structure.java
@@ -1,8 +1,20 @@
+import java.awt.*;
+
 public abstract class Structure
 {
-  public Structure (int x, int y, Image i, Size s) {}
-  
-  public abstract void draw (Graphics2D) {}
-  
-  public abstract void explode () {}
+  int x, y;
+  Image i;
+  Dimension d;
+
+  public Structure (int px, int py, Image pi, Dimension pd) //p for parameter
+  {
+	  x = px;
+	  y = py;
+	  i = pi;
+	  d = pd;
+  }
+
+  public abstract void draw (Graphics2D g);
+
+  public abstract void explode ();
 }
