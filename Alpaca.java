d281c05b2c234881566f466e585fb980e4398584
Jakepodell <Jake@jakepodell.com>
2014-05-27 09:55:37 -0400

    Update Alpaca.java

--- Alpaca.java
+++ Alpaca.java
@@ -1,7 +1,28 @@
-public class Alpaca
-{
-  public void draw (Graphics 2D)
-  public void rorateMouth ()
-  public void fluctuatePower ()
-  public Ammo launch ()
+import java.awt.Graphics2D;
+import java.awt.Point;
+
+public class Alpaca{
+	
+	private int power;
+	private int theta;
+	private int ammo;
+	
+	
+	public void draw(Graphics2D g){
+		  
+	}
+	public void rorateMouth(){
+		  
+	}
+	public void fluctuatePower(){
+		  
+	}
+	public Ammo launch(){
+		  
+	}
+	  
+	public static final Point ARILOC = new Point(50,200);
+	public static final int HEIGHT = 300;
+	public static final int WIDTH = 100;
+	public static final Point MOUTHLOC = new Point(ARILOC.x+WIDTH/2,ARILOC.y+HEIGHT/2);
 }
