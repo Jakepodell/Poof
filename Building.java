af3d194d4410279e201a8f886912c8d0b4d8dcf6
kevingreer <kevingreer@me.com>
2014-06-04 10:25:42 -0400

    Update Building.java

--- Building.java
+++ Building.java
@@ -1,13 +1,29 @@
 import java.awt.*;
 
+
 public class Building extends Structure
 {
-	public Building(int x, int y, Image i, Dimension d)
+	public Building(int x, int y, String imageName, Dimension d)
 	{
-		super(x, y, i, d);
+		super(x, y, imageName, d);
 	}
 	public void explode ()
 	{
+
+	}
+
+	public int getX()
+	{
+		return x;
+	}
+	public int getY()
+	{
+		return y;
+	}
+
+	public Rectangle getRect()
+	{
+		return (new Rectangle (x, y, (int) d.getWidth(), (int) d.getHeight()));
 	}
 	public void draw (Graphics2D g)
 	{
