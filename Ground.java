00f636029caeb18414921fdc192268d773a00cd9
johnmottole <johnmottole@gmail.com>
2014-06-05 10:24:39 -0400

    Update Ground.java

--- Ground.java
+++ Ground.java
@@ -2,12 +2,13 @@ import java.awt.Rectangle;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 public class Ground extends Rectangle {
-	public Ground(int x, int y, int width, int height){
+	public Ground(int x, int y, int width, int height)
+	{
 		super(x,y,width,height);
 	}
-//	public static Ground getGround(){
+/*	public static Ground getGround(){
 //		return new Ground(10,550,1300,75);
-//	}
+	}*/
 	public void draw(Graphics2D g)
 	{
 		g.drawRect(this);
