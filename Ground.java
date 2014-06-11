d4978816fabe57659d25d8e99712224cb78cb0f4
johnmottole <johnmottole@gmail.com>
2014-06-11 10:14:59 -0400

    Update Ground.java

--- Ground.java
+++ Ground.java
@@ -24,9 +24,13 @@ public class Ground{
 			theGrounds.add(new Rectangle(400,850,100,50));
 			theGrounds.add(new Rectangle(500,800,700,100));
 			theGrounds.add(new Rectangle(1000,500,75,300));
-		}else
+		}
+		else if (levelNumber == 3)
 		{
-
+			theGrounds.add(new Rectangle(0,800,500,100));
+			theGrounds.add(new Rectangle(500,890,200,10));
+			theGrounds.add(new Rectangle(700,800,900,100));
+			theGrounds.add(new Rectangle(1200,400,100,400));
 		}
 	}
 	public void draw(Graphics2D g)
