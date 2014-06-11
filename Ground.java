28d088e13068869d9d47eb17c89f9c2d2dd85de1
johnmottole <johnmottole@gmail.com>
2014-06-11 09:54:44 -0400

    Update Ground.java

--- Ground.java
+++ Ground.java
@@ -13,17 +13,18 @@ public class Ground{
 		groundImage = img;
 		if (levelNumber == 1)
 		{
-			theGrounds.add(new Rectangle(0,800,500,200));
-			theGrounds.add(new Rectangle(600,800,200,200));
+			theGrounds.add(new Rectangle(0,600,500,100));
+			theGrounds.add(new Rectangle(500,690,100,10));
+			theGrounds.add(new Rectangle(600,600,400,100));
+
 		}
 		else if (levelNumber == 2)
 		{
-			theGrounds.add(new Rectangle(0,800,100,200));
-			theGrounds.add(new Rectangle(100,900,100,100));
-			theGrounds.add(new Rectangle(200,800,400,200));
-			theGrounds.add(new Rectangle
-		}
-		else
+			theGrounds.add(new Rectangle(0,800,400,100));
+			theGrounds.add(new Rectangle(400,850,100,50));
+			theGrounds.add(new Rectangle(500,800,700,100));
+			theGrounds.add(new Rectangle(1000,500,75,300));
+		}else
 		{
 
 		}
