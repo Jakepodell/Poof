45253e6a62f15d117bd4c31b429c1547e53b3d78
kevingreer <kevingreer@me.com>
2014-06-13 10:23:06 -0400

    Update MildlyIrritatedAlpacae.java

--- MildlyIrritatedAlpacae.java
+++ MildlyIrritatedAlpacae.java
@@ -1,53 +1,56 @@
-import java.awt.Color;
-import java.awt.Dimension;
-import java.awt.Graphics;
 import java.awt.Graphics2D;
-import java.awt.Shape;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.util.concurrent.CopyOnWriteArrayList;
-import javax.swing.JApplet;
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.Timer;
+import javax.swing.*;
 
 public class MildlyIrritatedAlpacae extends JApplet implements ActionListener, MouseListener{
-	public static final Dimension FRAMESIZE = new Dimension(800,800);
+	public static final Dimension FRAMESIZE = new Dimension(1200,800);
 	Timer t = new Timer(60,this);
 	Alpaca ari;
 	CopyOnWriteArrayList<Ammo> pellets = new CopyOnWriteArrayList<Ammo>();
 	Ground gr;
 	ParticleEngine pe;
 	CopyOnWriteArrayList<Building> buildings = new CopyOnWriteArrayList<Building>();
-	
-	public MildlyIrritatedAlpacae(){
+	JProgressBar powerBar= new JProgressBar(0, 100);
+	int powerR = 255, powerG, powerB = 66;
 
-
+	public void init(){
 		setVisible(true);
+		setLayout(null);
 		setContentPane(new drawingPanel());
 		ari = new Alpaca(3,"Ari.png","Ari Alpaca Mouth.png",this);
 		addMouseListener(this);
-		gr = Ground.getGround();
+		gr = new Ground (1);
 		pe = new ParticleEngine();
-		buildings.add(new Building (850, gr.y - 300, "building.jpg", new Dimension (75, 300)));
-		buildings.add(new Building (1000, gr.y - 500, "building.jpg", new Dimension (75, 500)));
+		buildings.add(new Building (850, gr.convertXtoY(850) - 300, "building.jpg", new Dimension (75, 300)));
+		buildings.add(new Building (1000, gr.convertXtoY(1000) - 500, "building.jpg", new Dimension (75, 500)));
 		setSize(FRAMESIZE);
 		t.start();
+		powerBar.setBounds(10, 10, 100, 10);
+		add(powerBar);
 	}
 	@Override
 	public void actionPerformed(ActionEvent arg0) {
 		for (Particle p : pe.getParticleList())
 			p.tick();
 		pe.removeOffScreenParticles();
+		ari.fluctuatePower();
+		powerBar.setValue(ari.getPower());
+		changeProgressColor();
+		powerBar.setForeground(new Color(powerR, 255 - powerG, powerB));
 		repaint();
 	}
 	public class drawingPanel extends JPanel{
 		public void paintComponent(Graphics g){
 			super.paintComponent(g);
 			Graphics2D g2 = (Graphics2D)g;
-			ari.draw(g2);
+			ari.draw(g2, 1);
+			g2.setColor(Color.black);
+			g2.setStroke(new BasicStroke(1));
 			g2.drawRect((int)ari.getMouseLoc().getX(), (int)ari.getMouseLoc().getY(),10,10);
 			for(Ammo a: pellets){
 				a.draw(g2);
@@ -59,11 +62,11 @@ public class MildlyIrritatedAlpacae extends JApplet implements ActionListener, M
 					pellets.remove(a);
 				}
 			}
-			pe.draw(g2);;
+			pe.draw(g2);
 			g2.setColor(Color.black);
-			g2.draw(gr);
+			gr.draw(g2);
 			for (Building b: buildings)
-				b.draw(g2);
+				b.draw(g2, 1);
 		}
 	}
 	@Override
@@ -89,5 +92,10 @@ public class MildlyIrritatedAlpacae extends JApplet implements ActionListener, M
 	public void mouseReleased(MouseEvent arg0) {
 		// TODO Auto-generated method stub
 
+	}
+	public void changeProgressColor()
+	{
+		if (ari.getPower() / 100.0 <= 1.0)
+			powerG = (int) (255 * (ari.getPower() / 100.0));
 	}
 }
